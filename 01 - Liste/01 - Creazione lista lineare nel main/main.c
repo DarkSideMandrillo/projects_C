96b17c7c42076f0c15446ffd4478450d2ba048c6
DarkSideMandrillo <tiziano.meneg@gmail.com>
2024-10-15 22:40:56 +0200

    list simple

--- 01 - Liste/01 - Creazione lista lineare nel main/main.c
+++ 01 - Liste/01 - Creazione lista lineare nel main/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct
+typedef struct Node
 {
     int data;
     struct Node *next;
@@ -10,26 +10,39 @@ typedef struct
 int main()
 {
     // Creo il nodo di TESTA
-    Node *head  = (Node*)malloc(sizeof(Node));
-    head->data = 10; // Gli assegno 10 come data
-    head->next=NULL; // Gli assegno NULL al puntatore del prox nodo
+    Node *head = (Node *)malloc(sizeof(Node));
+    head->data = 10;   // Gli assegno 10 come data
+    head->next = NULL; // Gli assegno NULL al puntatore del prox nodo
 
     // Creo il nodo SECONDO
-    Node *second = (Node*)malloc(sizeof(Node));
-    second->data=20; // Gli assegno 20 come data
-    second->next=NULL; // Gli assegno NULL al puntatore del prox nodo
+    Node *second = (Node *)malloc(sizeof(Node));
+    second->data = 20;   // Gli assegno 20 come data
+    second->next = NULL; // Gli assegno NULL al puntatore del prox nodo
 
     // Faccio puntare il nodo TESTA all'indirizzo del nodo SECONDO
-    head->next=second;
+    head->next = second;
 
     // Creo il nodo TERZO
-    Node *third  = (Node*)malloc(sizeof(Node));
-    third->data = 03; // Gli assegno 30 come data
-    third->next=NULL; // Gli assegno NULL al puntatore del prox nodo
+    Node *third = (Node *)malloc(sizeof(Node));
+    third->data = 30;   // Gli assegno 30 come data
+    third->next = NULL; // Gli assegno NULL al puntatore del prox nodo
 
     // Faccio puntare il nodo SECONDO all'indirizzo del nodo TERZO
-    second->next=third;
+    second->next = third;
 
+    second = 0;
+    third = 0;
 
+    // Stampo i dati della lista
+    Node *current = head;
+    while (current != NULL)
+    {
+        printf("Dato: %d\n", current->data);
+        current = current->next;
+    }
+
+    // Libero la memoria allocata
+    free(third);
+    free(second);
+    free(head);
 }
-
